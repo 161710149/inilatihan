08784800cd555789b684f2abcbe8f496c02b7e9c
rhidayat122 <151610243@smkassalaambandung.sch.id>
2017-10-24 08:49:44 +0400

    inilatihan

---
+++ index3.php
@@ -0,0 +1,13 @@
+<?php
+
+	class kuda{
+
+		public $kaki = 4;
+		public $makan = 'rumput';
+
+	}
+	$kuda1 = new kuda;
+	echo "Kuda berjalan dengan  :".$kuda1-> kaki .' kaki <br>' ;
+	echo "dan memakan :".$kuda1-> makan .'<br>' ;
+
+?>
